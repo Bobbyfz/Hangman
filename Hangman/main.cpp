214b8394422893bc248a2019398884531142d2a2
Bobby <Robert.feliz86@gmail.com>
2020-01-09 00:15:30 -0500

    Initial Commit

---
+++ Hangman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hangman
+//
+//  Created by robert feliz on 1/9/20.
+//  Copyright © 2020 robert feliz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
