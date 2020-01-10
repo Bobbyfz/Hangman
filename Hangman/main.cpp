6c0be1bce8233c67ea81f8e6f363882f48d8a374
Bobby <Robert.feliz86@gmail.com>
2020-01-10 00:13:27 -0500

    Added functions Used functions to receive the word and added the beginning of the loop

--- Hangman/main.cpp
+++ Hangman/main.cpp
@@ -2,14 +2,79 @@
 //  main.cpp
 //  Hangman
 //
-//  Created by robert feliz on 1/9/20.
+//  Created by robert feliz on 1/8/20.
 //  Copyright © 2020 robert feliz. All rights reserved.
 //
 
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void hangman(void);
+string pickWord(void);
+
+int main(int argc, const char * argv[])
+{
+    
+    string userName;
+    char choice;
+    
+    //loop to select the name
+    do
+    {
+    cout << "Welcome to Hangman, what is your name?\n";
+    getline(cin,userName);
+    
+    cout << "Your name is: "<<userName<<"\n";
+    cout<<"Is that correct?(y/n)\n";
+    
+    cin>>choice;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    }while (choice != 'y'&&choice != 'Y'&&choice != 'n'&&choice != 'N');
+    
+    hangman();
+    
     return 0;
+}
+
+void hangman(void)
+{
+    string word = pickWord();
+    string unknownWord(word.length(),'*');
+    
+    cout << unknownWord;
+}
+
+//function to randomly pick the word used for guessing
+string pickWord(void)
+{
+    string words[] =
+    {
+        "mere"
+        "laughable"
+        "mother"
+        "detail"
+        "attempt"
+        "chief"
+        "annoying"
+        "immense"
+        "changeable"
+        "train"
+        "unnatural"
+        "loutish"
+        "government"
+        "smell"
+        "berserk"
+    };
+    
+    srand(time(NULL));
+    
+    int randNum = rand() % 15 + 1;
+    
+    string mainWord = words[randNum];
+    return mainWord;
 }
